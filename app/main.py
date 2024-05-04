fde40644e0eb681a5382681c6392d45fc19e08d9
Alex Troianovskiy <168637534+Alex93Troy@users.noreply.github.com>
2024-05-04 23:17:41 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,26 @@
 from __future__ import print_function
 
+
 def format_linter_error(error: dict) -> dict:
     return {"line": error.get("line_number"),
             "column": error.get("column_number"),
             "message": error.get("text"),
-            "name": error.get("code"), "sourse": "flake8"}
+            "name": error.get("code"), "sourse": "flake8"
+            }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error)
-        for error in errors], "path": file_path,
-        "status": "failed"} if errors else {
-        "path": file_path, "status": "passed"}
+                       for error in errors], "path": file_path,
+            "status": "failed"} if errors else {
+        "path": file_path, "status": "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [{"errors": [format_linter_error(file) for file in value],
              "path": key,
              "status": format_single_linter_file(key, value).get("status")}
-            for (key, value) in linter_report.items()]
+            for (key, value) in linter_report.items()
+            ]
 
