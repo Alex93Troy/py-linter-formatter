e3ccbcbc0f2ff608d2bef23d6991ec1711a06b9d
Alex93Troy <troianovskiy93alex@gmail.com>
2024-05-04 22:42:55 +0200

    "fix space"

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 """ make my format linter"""
 
+
 def format_linter_error(error: dict) -> dict:
-    return {"line": error.get("line_number"), "column": error.get("column_number"),
-            "message": error.get("text"), "name": error.get("code"), "sourse": "flake8"}
+    'make funk for format error'
+    return {"line": error.get("line_number"),
+            "column": error.get("column_number"),
+            "message": error.get("text"),
+            "name": error.get("code"), "sourse": "flake8"}
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"path": file_path, "status": "passed"} if not errors else\
-        {"errors": [format_linter_error(error) for error in errors],
-        "path": file_path, "status": "failed"}
+    'make func for check status error and create list errors'
+    return {"errors": [format_linter_error(error)
+    for error in errors], "path": file_path, "status": "failed"} \
+        if errors else {"path": file_path, "status": "passed"}
+
 
 def format_linter_report(linter_report: dict) -> list:
+    'func for format all file with errors'
     return [{"errors": [format_linter_error(file) for file in value],
              "path": key, "status": format_single_linter_file(key, value).get("status")}
             for (key, value) in linter_report.items()]
